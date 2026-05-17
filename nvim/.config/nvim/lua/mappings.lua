2a786b514a594d4f02bef1e8a4e07727f1b5e15a
MattyOstrowsky <matty@ostrowsky.dev>
2026-05-17 16:08:38 +0200

    feat: add <leader>md shortcut to toggle render-markdown

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -8,3 +8,6 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- toggle render-markdown
+map("n", "<leader>md", "<cmd>RenderMarkdown toggle<CR>", { desc = "Toggle markdown rendering" })
